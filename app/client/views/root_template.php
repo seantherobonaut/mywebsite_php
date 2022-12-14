8d5cf3c0bd4d5fb68e30e708225342e57caabb3a
Sean Leapley <seantherobonaut@gmail.com>
2022-12-14 01:17:59 +0000

    save progress

--- app/client/views/root_template.php
+++ app/client/views/root_template.php
@@ -10,12 +10,10 @@
         <title><?php echo ucfirst($page);?></title>            
         <link rel="shortcut icon" href="/app/client/images/awesome_face.ico">
 
-        <!-- Jquery and Bootstrap -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>        
-        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">        
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
+        <!-- Bootstrap 5 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        
         <!-- Font for icons -->
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
