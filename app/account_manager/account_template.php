649ffc82cd804ea17947b20dc391661fa8103cad
Sean Leapley <seantherobonaut@gmail.com>
2023-09-29 23:45:20 -0700

    woops, minor html issue

--- app/account_manager/account_template.php
+++ app/account_manager/account_template.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=0device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
             
         <meta name="author" content="Sean Leapley">
         <meta name="description" content="Profile Interface">
